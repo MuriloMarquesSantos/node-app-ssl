3a3a36e8f267c1a532adff3f58573634839bb187
MuriloMarquesSantos <murilommms@gmail.com>
2022-02-05 14:40:45 -0300

    Adding simple express server

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-(function sayHello() {
-    console.log("Hello world");
-})();
+const express = require('express');
+
+const app = express();
+
+app.get("/", (request, response) => { 
+    return response.send("Hello world");
+});
+
+app.listen(3333, () => console.log("Listening on port 3333"));
